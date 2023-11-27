027ce6af7ca581c1b6b5768765d3fc3fa07c6445
Albwer Szczepanik <szamipl@gmail.com>
2023-11-27 07:29:08 -0500

    login, add users te sprawy

--- myproject/calculator/urls.py
+++ myproject/calculator/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import hello
+from .views import calc, get_users,add_user,login
 
 urlpatterns = [
     path('hello/<int:number>', hello),
     path('calc',calc),
+    path('user',get_users),
+    path('add_user',add_user),
+    path('login',login),
 ]
--- myproject/calculator/views.py
+++ myproject/calculator/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-from django.views.decoration.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
+
+from calculator.models import User
 
 def hello(request, number):
     return HttpResponse(f"Hello{number}")
@@ -12,4 +14,42 @@ def calc(request):
     data = json.loads(request.body)
     if data["operation"] == "+":
         result = data['a'] + data['b']
+    elif data["operation"] == "*":
+        result = data['a'] * data['b']
     return HttpResponse(f"{result}")
+    
+def get_users(request):
+    users = User.objects.all()
+    user_data = []
+    for user in users:
+        user_data.append(user.username)
+    return JsonResponse({"Users":user_data})
+
+@csrf_exempt
+def add_user(request):
+    data = json.loads(request.body)
+    username = data.get("username")
+    password = data.get("password")
+    
+    users = User.objects.all()
+    for existing_user in users:
+        if existing_user.username == username:
+            return HttpResponse("User already exist",status=400)
+    
+    user = User(username=username,password=password)
+    user.save()
+    
+    return JsonResponse({"username":user.username,"password":user.password})
+
+@csrf_exempt
+def login(request):
+    data = json.loads(request.body)
+    username = data.get("username")
+    password = data.get("password")
+    
+    try:
+        user = User.objects.get(username=username,password=password)
+        return HttpResponse("user exists!",status=200)
+    except:
+        return HttpResponse("user dose not exist!",status=400)
+    
